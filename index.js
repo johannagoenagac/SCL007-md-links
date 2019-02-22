9dc1356eeea9351a0c1f098476f6ce87be576585
johannagoenagac <johannagoenagac@gmail.com>
2019-02-22 14:58:48 -0300

    se instalan librerias

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-module.exports = () => {
-  // ...
-};
+// import { fstat } from "fs";
+
+// module.exports = () => {
+//   // ...
+// };
+
+const fs = require("fs");
+
+// const readFile = fs.readFile("./README.md", "utf-8", (error, data)=>{
+//   if(error){
+//     console.log("error")
+//   }else{
+//     console.log(data)  
+//   }
+// });
+
+fs.readFile('./README.md', function (err, data){
+  if(err){
+    console.log(err);
+  }
+  console.log(data.toString());
+})
+
+
+
+
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,25 @@
-const mdLinks = require('../');
+// const mdLinks = require('../');
 
 
-describe('mdLinks', () => {
+// describe('mdLinks', () => {
 
-  it('should...', () => {
-    console.log('FIX ME!');
+//   it('should...', () => {
+//     console.log('FIX ME!');
+//   });
+
+// });
+const App = require('../app.js');
+
+describe('Leer un directorio', () => {
+    //Así se testea algo con promesas
+    test('Debería ser capaz de leer el directorio del programa', () => {
+        expect.assertions(1); //el número indica la cantidad de expects que tienes en tu test
+        return App.readDirPromise(__dirname)
+            .then((files) => {
+                expect(files).toContain('app.test.js');
+            }).catch((error) => {
+
+            });
+    });
+
   });
-
-});
