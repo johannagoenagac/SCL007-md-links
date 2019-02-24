9f9fa84717c28b86787ae8e4288a70438ac456b3
johannagoenagac <johannagoenagac@gmail.com>
2019-02-24 18:46:12 -0300

    lee archivo, encuentra links, imrpime links y status

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 //   // ...
 // };
 
-const fs = require("fs");
+
 
 // const readFile = fs.readFile("./README.md", "utf-8", (error, data)=>{
 //   if(error){
@@ -14,13 +14,57 @@ const fs = require("fs");
 //   }
 // });
 
-fs.readFile('./README.md', function (err, data){
-  if(err){
-    console.log(err);
+// const fs = require("fs");
+
+// fs.readFile('./README.md', function (err, data){
+//   if(err){
+//     console.log(err);
+//   }
+//   console.log(data.toString());
+// })
+
+const fs = require('fs'),
+readline = require('readline');
+const fetch = require('node-fetch');
+
+const rd = readline.createInterface({
+    input: fs.createReadStream('./README.md'),
+    // output: process.stdout,
+    // console: false
+});
+
+let counterLine = 0;
+
+rd.on('line', function(line) {
+    // console.log(line);
+
+counterLine ++;
+const filePattern = line
+const pattern = /((telnet:\/\/|file:\/\/|https:\/\/|http:\/\/|ftp:\/\/|www\.)[^\s]+)/;
+const lineMatch = filePattern.match(pattern);
+if (lineMatch != null){
+
+let result=[]
+result.push(lineMatch[0])
+for (let i=0; i<result.length;i++){
+  // console.log(+counterLine + ': '   +result[i]);  }
+
+fetch(result[i])
+.then((res)  =>{
+  
+  console.log( res.url)
+  console.log(res.status, res.statusText)
+});
+
+     
+
+    } 
+
   }
-  console.log(data.toString());
-})
+
+});
 
 
-
+// path.extname('README.md');
+// // Returns: '.md'
 
